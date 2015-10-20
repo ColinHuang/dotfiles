3533c0198ba1b4bfc70a729ca4c82f8b423c80b5
Colin Huang <sidawei@gmail.com>
2015-10-20 01:27:26 +0000

    Add tigrc

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim zshrc zsh_aliases gitconfig"    # list of files/folders to symlink in homedir
+files="vimrc vim zshrc zsh_aliases gitconfig tigrc"    # list of files/folders to symlink in homedir
 
 ##########
 
