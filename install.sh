c38fbe7f73be5802bf300d1b73232952827e38a9
Colin Huang <sidawei@gmail.com>
2018-02-27 08:57:05 +0000

    revert dotfiles_old

--- install.sh
+++ install.sh
@@ -7,8 +7,13 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
 files="vimrc vim zshrc zsh_aliases gitconfig tigrc pip pypirc"    # list of files/folders to symlink in homedir
 
+# create dotfiles_old in homedir   
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."    
+mkdir -p $olddir   
+echo "done"
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
@@ -17,6 +22,8 @@ echo "done"
 
 # Create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"  
+    mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
