2c596817467fd7c8d8271a9abc75b89cfccc7b13
Colin Huang <sidawei@gmail.com>
2018-08-31 20:07:21 +0800

    fix permission

--- install_tag.sh
+++ install_tag.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get -y install zsh git-core tree curl ctags silversearcher-ag htop autojump g++ exuberant-ctags \
       cscope indent cppcheck build-essential fakeroot tig highlight jq lrzsz
-sudo wget https://github.com/aykamko/tag/releases/download/v1.4.0/tag_linux_amd64.tar.gz -O - | tar -xz -C /bin/
+wget https://github.com/aykamko/tag/releases/download/v1.4.0/tag_linux_amd64.tar.gz -O - | sudo tar -xz -C /bin/
 
